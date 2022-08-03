a72cc6490f8a475fe7cc351cd647b1e450b37ebb
AASanusi <alaminm.sanusi@gmail.com>
2022-08-03 20:45:51 +0000

    Add welcome function for the application introduction

--- run.py
+++ run.py
@@ -19,8 +19,14 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('weekly_playlist')
 
-week_one = SHEET.worksheet('week_one')
+def weekly_playlist_welcome():
+    """
+    Introducing and informing the user about
+    the application.
+    """
+    print(Back.YELLOW + 'Welcome music lovers to the weekly music playlist!\n')
+    print(Fore.GREEN + 'Finding and listening to music is life.')
+    print(Fore.GREEN + 'Use this playlist to check out songs for each week')
+    print(Fore.GREEN + Style.BRIGHT + 'and add your own songs to any week.')
 
-data = week_one.get_all_values()
 
-print(data)
